2232b8190969c21c04b60080bb836ecfaecb2347
madagasikara4 <miaroandy25@gmail.com>
2022-12-20 17:49:18 +0300

    Update UserService.java

--- src/main/java/com/service/UserService.java
+++ src/main/java/com/service/UserService.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.repository.UserRepository;
 
-import javax.rmi.CORBA.Util;
 import java.util.List;
 import java.util.ArrayList;
 
